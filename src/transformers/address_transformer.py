36f0aec29b1f8e2f016548c7502c05d211196616
Rob K <robertdkauffman@gmail.com>
2025-01-12 08:38:43 -0700

    Add typing

--- src/transformers/address_transformer.py
+++ src/transformers/address_transformer.py
@@ -1,10 +1,51 @@
+from logging import Logger
+from typing import Any, Dict
+
 from .base_transformer import BaseTransformer
 
 
 class AddressTransformer(BaseTransformer):
+    """
+    Transformer for PAN address configurations.
+    Converts PAN address format to Versa address format.
+    """
+
     @staticmethod
-    def transform(address: dict, logger) -> dict:
-        """Transform an address entry to the desired format."""
+    def transform(
+        self, data: Dict[str, Any], logger: Logger, **kwargs: Any
+    ) -> Dict[str, Any]:
+        """
+        Transform a PAN address entry to Versa format.
+
+        Args:
+            data: Source address configuration data containing:
+                - name: Address name
+                - ip-netmask: IP address with netmask
+                - description: Optional description
+            logger: Logger instance for logging transformation operations
+            **kwargs: Additional parameters (unused in this transformer)
+
+        Returns:
+            Dict[str, Any]: Transformed address configuration in Versa format
+
+        Example input:
+        {
+            "name": "web_server",
+            "ip-netmask": "192.168.1.100/24",
+            "description": "Web Server"
+        }
+
+        Example output:
+        {
+            "address": {
+                "name": "web_server",
+                "description": "Web Server",
+                "ipv4-prefix": "192.168.1.100/24"
+            }
+        }
+        """
+        address = data
+
         logger.debug(
             f"Initial address details: (Name={address['name']}, IP/Netmask={address['ip-netmask']}, Description={address.get('description', '')}"
         )
