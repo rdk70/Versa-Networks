7ed4f9c414cf06ea0556fdd12e948fed13fdd811
Rob K <robertdkauffman@gmail.com>
2025-02-05 20:50:46 -0700

    merge fix

--- Security-Traffic-Generator/Security-Traffic-Generator.py
+++ Security-Traffic-Generator/Security-Traffic-Generator.py
@@ -135,9 +135,7 @@ def main():
         separator = "~" * 78
         print(f"{Colors.GREEN}{separator}{Colors.NONE}")
         print(f"{Colors.GREEN}Traffic generator started{Colors.NONE}")
-        print(
-            f"{Colors.GREEN}This script will run indefinitely. Ctrl+C to stop.{Colors.NONE}"
-        )
+        print(f"{Colors.GREEN}This script will run indefinitely. Ctrl+C to stop.{Colors.NONE}")
         print(f"{Colors.GREEN}{separator}{Colors.NONE}")
 
         # Configuration info
@@ -165,13 +163,7 @@ def main():
             print("\nStarting Web Traffic Generation...")
             for url in urls:
                 print(f"\nBrowsing from starting URL: {url}")
-<<<<<<< HEAD
                 # random_depth = random.randint(config_data["min_depth"], config_data["max_depth"])
-=======
-                random_depth = random.randint(
-                    config_data["min_depth"], config_data["max_depth"]
-                )
->>>>>>> 757845b4300c7f8e7f9a9dda7ed88f316fe1841d
                 do_request(url)
 
         # Display final statistics
